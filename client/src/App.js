aeae69a2f6aaeda5001f696747cde5be82da45b3
KadafiBelajar <bibibibu9900@gmail.com>
2025-09-28 15:19:24 +0700

    debug: Simplify App component to test build process

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,11 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('/api')
-      .then(res => res.json())
-      .then(data => setData(data.message))
-      .catch(err => console.error("Error fetching data: ", err));
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Math Game</h1>
-        <p>{data ? data : "Loading..."}</p>
+        <p>Build process test.</p>
       </header>
     </div>
   );
